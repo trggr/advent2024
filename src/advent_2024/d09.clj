b25f5007d8fa64de371be5be2fcb9efa422b3d3b
trggr <tashepkov@gmail.com>
2025-01-06 11:32:43 -0500

    Day 9, part 2 works

--- src/advent_2024/d09.clj
+++ src/advent_2024/d09.clj
@@ -6,7 +6,6 @@
               (map str)
               (map parse-long)
               (partition-all 2)))
-;; ((2 3) (3 3) (1 3) (3 1) (2 1) (4 1) (4 1) (3 1) (4 0) (2))
 
 (defn arange
   "Assign v to m for block of keys between from and from + len"
@@ -30,61 +29,38 @@
                            tmp
                            (arange tmp \. end gap)))))))
 
-(reduce (fn [acc [f g]] (+ acc f (or g 0))) 0 inp)
+(defn checksum
+  [coll]
+  (reduce (fn [acc [k v]]
+            (if (= v \.) acc (+ acc (* k v))))
+          0
+          coll))
 
-;; (apply str (vals block-map))
-;; "00...111...2...333.44.5555.6666.777.888899"
-;;  2 3  3  3  13  3  12 14   14   13  14  02)
-;;  01---+----1----+----2----+----3----+----4----+----5"
+(defn part1
+  [blocks]
+  (->> (map (fn [from to] [from to])
+            (->> blocks (filter (fn [[_k v]] (not= v \.))) (map first) reverse)
+            (->> blocks (filter (fn [[_k v]] (= v \.))) (map first)))
+       (reduce (fn [acc [from to]]
+                 (if (>= to from)
+                   acc
+                   (assoc acc to (get acc from) from \.)))
+               blocks)
+       checksum))
 
 
-;; part 1
-(->> (map (fn [from to] [from to])
-          (->> blocks (filter (fn [[_k v]] (not= v \.))) (map first) reverse)
-          (->> blocks (filter (fn [[_k v]] (= v \.))) (map first)))
-     (reduce (fn [acc [from to]]
-               (if (>= to from)
-                 acc
-                 (assoc acc to (get acc from) from \.)))
-             blocks)
-     (reduce (fn [acc [k v]] (if (= v \.) acc (+ acc (* k v)))) 0))
+;; map of gaps, where each gap is a vector of [address length]
+(def gaps (->> inp
+               (reduce (fn [[mp addr :as acc] [file gap]]
+                         (let [addr (+ addr file)]
+                           (if-not gap
+                             acc
+                             (if (zero? gap)
+                               [mp addr]
+                               [(assoc mp addr gap) (+ addr gap)]))))
+                       [(sorted-map) 0])
+               first))
 
-;; map of free chunks, where each chunk is a vector of [chunk-address chunk-len]
-(def chunks  (loop [acc       (sorted-map)
-                    addr       0
-                    coll       inp]
-               (if-not (seq coll)
-                 acc
-                 (let [[[filen gaplen] & more] coll
-                       addr                    (+ addr filen)]
-                   (cond (nil? gaplen)   acc
-                         (zero? gaplen)  (recur acc addr more)
-                         :else           (recur (assoc acc addr gaplen)
-                                                (+ addr gaplen)
-                                                more))))))
-
-;; files
-;;----------------------;;  {0 2, 1 3, 2 1, 3 3, 4 2, 5 4, 6 4, 7 3, 8 4, 9 2}
-;; {0 {:addr 0, :len 2},
-;;  1 {:addr 5, :len 3},
-;;  2 {:addr 11, :len 1},
-;;  3 {:addr 15, :len 3},
-;;  4 {:addr 19, :len 2},
-;;  5 {:addr 22, :len 4},
-;;  6 {:addr 27, :len 4},
-;;  7 {:addr 32, :len 3},
-;;  8 {:addr 36, :len 4},
-;;  9 {:addr 40, :len 2}}
-
-chunks
-;; {  2 3,      8 3,     12 3,     18 1,     21 1,     26 1,     31 1,     35 1}
-
-;; block-map
-;; "00...111...2...333.44.5555.6666.777.888899"
-;;  2 3  3  3  13  3  12 14   14   13  14  02)
-;;  01---+----1----+----2----+----3----+----4----+----5"
-
-;; inp-pairs ((2 3) (3 3) (1 3) (3 1) (2 1) (4 1) (4 1) (3 1) (4 0) (2))
 
 (def files (->> inp
                 (reductions (fn [acc [a b]] (+ acc a (or b 0))) 0)
@@ -95,42 +71,36 @@ chunks
                 (reduce (fn [acc [file-id addr len]] (assoc acc file-id {:addr addr :len len}))
                         (sorted-map))))
 
-;; {0 {:addr 0,  :len 2},
-;;  1 {:addr 5,  :len 3},
-;;  2 {:addr 11, :len 1},
-;;  3 {:addr 15, :len 3},
-;;  4 {:addr 19, :len 2},
-;;  5 {:addr 22, :len 4},
-;;  6 {:addr 27, :len 4},
-;;  7 {:addr 32, :len 3},
-;;  8 {:addr 36, :len 4},
-;;  9 {:addr 40, :len 2}}
-
-(def output
+(defn part2
+  [blocks files gaps]
   (->> files
        keys
        reverse
-       (reduce (fn [[blx chnx :as acc] id]
-                 (let [file   (get files id)
+       (reduce (fn [[blx gapps :as acc] id]
+                 (let [file               (get files id)
                        {:keys [addr len]} file
-                       chunk  (->> chnx
-                                   (filter (fn [[_ clen]] (>= clen len)))
-                                   first)]
-                   (if-not chunk
+                       gap                (->> gapps
+                                               (filter (fn [[_ free]] (>= free len)))
+                                               first)]
+                   (if (not gap)
                      acc
-                     (let [[caddr clen] chunk
-                           blx          (-> (arange blx id caddr len)
-                                            (arange     \. addr  len))
-                           remain       (- clen len)
-                           chnx         (dissoc chnx caddr)
-                           chnx         (if (zero? remain)
-                                          chnx
-                                          (assoc chnx (+ caddr len) remain))]
-                       [blx chnx]))))
-               [blocks chunks])
+                     (let [[to free] gap]
+                       (if (>= to addr)
+                         acc
+                         (let [blx      (-> blx
+                                            (arange id to len)
+                                            (arange \. addr len))
+                               remain   (- free len)
+                               gapps     (cond-> gapps
+                                           true          (dissoc to)
+                                           (pos? remain) (assoc (+ to len) remain))]
+                           [blx gapps]))))))
+               [blocks gaps])
        first
-       (reduce (fn [acc [k v]] (if (= v \.) acc (+ acc (* k v)))) 0)))
+       checksum))
 
 
-;; too high:      8654184283366
-;; right answer = 6469636832766
+(defn main
+  []
+  (println (part1 blocks)
+           (part2 blocks files gaps)))
