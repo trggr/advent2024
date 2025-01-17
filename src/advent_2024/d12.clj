66a82eba9c03a286aad3509bec1fa1ce2cd74fe3
trggr <tashepkov@gmail.com>
2025-01-16 21:07:23 -0500

    working towards day 12, part 2

--- src/advent_2024/d12.clj
+++ src/advent_2024/d12.clj
@@ -2,7 +2,7 @@
   (:require [advent-2024.lib :as lib]
             [clojure.string  :as str]))
 
-(def inp (->> "d12"
+(def inp (->> "d12-test1"
               lib/read-file
               str/split-lines
               (map-indexed (fn [i s] [i (zipmap (range) (seq s))]))
@@ -13,16 +13,16 @@
                                 mp))
                       (sorted-map))))
 
-(def dirs {:up     [-1 0],
-           :right  [0 1],
-           :down   [1 0],
-           :left   [0 -1]})
+(def dirs {:n   [-1 0],
+           :e   [0 1],
+           :s   [1 0],
+           :w   [0 -1]})
 
 (defn add
   [[x1 y1] [x2 y2]]
   [(+ x1 x2) (+ y1 y2)])
 
-(defn neighbors
+(defn plot-neighbors
   "Takes a point returns neighboring points with the same value"
   [p]
   (let [v  (get inp p)]
@@ -45,7 +45,7 @@
       plot
       (let [[p & more]  todo
             nbs         (->> p
-                             neighbors
+                             plot-neighbors
                              (remove (fn [x] (contains? plot x))))]
         (recur (reduce (fn [acc x] (assoc acc x step)) plot nbs)
                (concat more nbs)
@@ -70,7 +70,7 @@
   [plot]
   (->> plot
        keys
-       (map neighbors)
+       (map plot-neighbors)
        (map count)
        (map (partial - 4))
        (reduce +)))
@@ -86,3 +86,97 @@
     (println (->> plots (map price) (reduce +)))))
 
 ;; (main)
+
+
+;;---- part2
+
+(defn all-neighbors
+  "Takes a point returns neighboring points with the same value"
+  [p]
+  (->> dirs
+       vals
+       (map (partial add p))
+       (filter (fn [x] (contains? inp x)))))
+
+
+(defn determine-plot4
+  "Takes p and returns a vector of two: a
+   plot to which p belongs and bordering points"
+  [p]
+  (let [crop (get inp p)]
+    (loop [plot    (sorted-set)
+           border  (sorted-set)
+           todo [p]]
+      (if-not (seq todo)
+        [plot border]
+        (let [[x & more] todo]
+          (if (= crop (get inp x))
+            (recur (conj plot x)
+                   border
+                   (concat more (->> x all-neighbors (remove plot))))
+            (recur plot
+                   (conj border x)
+                   more)))))))
+
+; (determine-plot4 [1 2])
+
+(defn find-plots2
+  [inp]
+  (loop [plots   []
+         unalloc (-> inp keys set)]
+    (if-not (seq unalloc)
+      plots
+      (let [p     (first unalloc)
+            rc (determine-plot4 p)]
+        (recur (conj plots rc)
+               (apply disj unalloc (first rc)))))))
+
+(def plot-border-pairs (find-plots2 inp))
+
+(def plots (mapv first plot-border-pairs))
+(def borders (mapv second plot-border-pairs))
+(def plot-map
+  (->> (for [i      (range (count plots)),
+             p      (get plots i)]
+         [p i])
+       (into (sorted-map))))
+;; {[0 0] 1
+;;  [0 1] 1
+;;  [0 2] 1
+;;  [0 3] 1
+;;  [1 0] 2
+;;  [1 1] 2
+;;  [1 2] 0
+;;  [1 3] 4
+;;  [2 0] 2
+;;  [2 1] 2
+;;  [2 2] 0
+;;  [2 3] 0
+;;  [3 0] 3
+;;  [3 1] 3
+;;  [3 2] 3
+;;  [3 3] 0}
+plot-border-pairs
+
+[[#{[1 2] [2 2] [2 3] [3 3]} #{[0 2] [1 1] [1 3] [2 1] [3 2]}]
+ [#{[0 0] [0 1] [0 2] [0 3]} #{[1 0] [1 1] [1 2] [1 3]}]
+ [#{[1 0] [1 1] [2 0] [2 1]} #{[0 0] [0 1] [1 2] [2 2] [3 0] [3 1]}]
+ [#{[3 0] [3 1] [3 2]} #{[2 0] [2 1] [2 2] [3 3]}]
+ [#{[1 3]} #{[0 3] [1 2] [2 3]}]]
+
+(->> plot-border-pairs
+     (map-indexed (fn [idx [_plot border]]
+                    [idx (map plot-map border)])))
+
+(->> (for [b borders] (map plot-map b))
+     (map-indexed (fn [idx x]
+                    (interleave (repeat idx) x))))
+
+(->>
+ (for [i (range (count borders))
+       :let [border (get borders i),
+             rels   (map plot-map border)]
+       rel rels]
+   [i rel])
+ distinct
+ sort)
